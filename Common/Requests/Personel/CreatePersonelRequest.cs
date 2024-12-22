0642cb51f81d73273ee49a85429f4e3a0f17334c
Metehan Öztürk <mete_pukkada@outlook.com>
2024-12-22 21:11:06 +0300

    #LAST_LAST_FIX

--- Common/Requests/Personel/CreatePersonelRequest.cs
+++ Common/Requests/Personel/CreatePersonelRequest.cs
@@ -1,11 +1,9 @@
-﻿using Common.Requests.kisi;
-
-namespace Common.Requests.Personel
+﻿namespace Common.Requests.Personel
 {
     public class CreatePersonelRequest
     {
-        public string? Ad;
-        public string? Soyad;
+        public string? Ad { get; set; }
+        public string? Soyad { get; set; }
         public Guid? KisiId { get; set; }
         public required string Pozisyon { get; set; }
 
